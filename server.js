18f176a7c1f49ad2e807af9111a3dbb12cbd135c
jonathanslima <jonathan633.jl@gmail.com>
2019-04-10 00:40:31 -0300

    change name file

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 /*/ Requires */
-// require('./app/routes/node.routes.js')(app);
-
-
+require('./app/routes/note.routes.js')(app);
 
 /*/ Connecting to the database*/
 mongoose.connect(dbConfig.url, {
@@ -33,22 +31,6 @@ app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
-    const notes = require('./app/controllers/note.controller.js');
-
-    // Create a new Note
-    app.post('/notes', notes.create);
-
-    // Retrieve all Notes
-    app.get('/notes', notes.findAll);
-
-    // Retrieve a single Note with noteId
-    app.get('/notes/:noteId', notes.findOne);
-
-    // Update a Note with noteId
-    app.put('/notes/:noteId', notes.update);
-
-    // Delete a Note with noteId
-    app.delete('/notes/:noteId', notes.delete);
 
 /*/ listen for requests*/
 app.listen(3000, () => {
